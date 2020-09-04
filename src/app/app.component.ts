b8783edd0d6549739354af9bf30ef8bd40217ca2
rreddyNg <ravindrap@hotmail.com>
2020-09-04 12:14:31 -1000

    Weather app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Subject, Observable } from 'rxjs';
+import { takeUntil, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'weather-app';
+export class AppComponent implements OnInit, OnDestroy {
+  private unsubscribe$ = new Subject<void>();
+
+  countries = [
+    {
+      name: 'United Kingdom',
+      cities: ['London', 'Warwick', 'Birmingham']
+    },
+    {
+      name: 'United States',
+      cities: ['New York', 'Chicago', 'Washington', 'Honolulu']
+    },
+    {
+      name: 'Australia',
+      cities: ['Sydney', 'Adelaide', 'Melbourne']
+    },
+    {
+      name: 'India',
+      cities: ['Mumbai', 'Bengaluru', 'Hyderabad']
+    }
+  ];
+
+  countryControl: FormControl;
+  cityControl: FormControl;
+
+  cities$: Observable<string>;
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    this.cityControl = new FormControl('');
+    this.cityControl.valueChanges
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe(value => {
+        this.router.navigate([value]);
+      });
+
+    this.countryControl = new FormControl('');
+
+    this.cities$ = this.countryControl.valueChanges.pipe(
+      map(country => country.cities)
+    );
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
 }
